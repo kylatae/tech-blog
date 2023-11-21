927101e57399ea9931ebeff317e695f8d95a0dea
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-11-20 21:05:36 -0600

    Changelog - Added Login and Signup Pages (No JS yet) - Formatted navbar layout - Added style.css and started adding styles

--- controllers/handlebars/display.handlebars.routes.js
+++ controllers/handlebars/display.handlebars.routes.js
@@ -11,4 +11,24 @@ router.get('/', async (req, res) => {
 });
 
 
+router.get('/login', async (req, res) => {
+  try{
+    res.render('login');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  
+});
+
+router.get('/signup', async (req, res) => {
+  try{
+    res.render('signup');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  
+});
+
 module.exports = router;
