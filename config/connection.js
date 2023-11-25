c654cf6a71067c76eec838c2e4fd4bd4e09f4472
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-11-24 23:46:44 -0600

    Updated connections for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 const Sequelize = require('sequelize');
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    // Database location
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(process.env.DB_NAME,
+  process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
@@ -14,3 +12,5 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
+
+
